54a7db55c007fc2e760eccb732d2a9b023ab643b
NerdsyChamplain <joshua.scalia@mymail.champlain.edu>
2024-02-13 15:16:25 -0500

    Worked on turret rotation. It currently isn't rotating properly, change rotation from being in update to being triggered by something.

--- PlatformerGMD200/Assets/scripts/Turret.cs
+++ PlatformerGMD200/Assets/scripts/Turret.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
+    public Transform shotPoint;
+    public GameObject turHead;
+    public Transform rotPoint;
+    public PlayMove player;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +17,11 @@ public class Turret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        //rotPoint.transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        turHead.transform.RotateAround(rotPoint.position, Vector3.forward, angle);
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
