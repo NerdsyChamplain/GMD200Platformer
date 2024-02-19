c575969b6e41cf56dc7c682bad6e2163fb3fd034
NerdsyChamplain <joshua.scalia@mymail.champlain.edu>
2024-02-19 18:02:11 -0500

    Adjusted grappling hook
    Reverted it so it works but without detecting collisions at all

--- PlatformerGMD200/Assets/scripts/Grapple.cs
+++ PlatformerGMD200/Assets/scripts/Grapple.cs
@@ -7,9 +7,7 @@ public class Grapple : MonoBehaviour
     public Camera mainCamera;
     public LineRenderer _lineRenderer;
     public DistanceJoint2D _distanceJoint;
-    public LayerMask hitLayer;
     //code aquired from: https://www.youtube.com/watch?v=P-UscoFwaE4
-    //edited by: Josh Scalia to make it stop on colliding with an object
     // Start is called before the first frame update
     void Start()
     {
@@ -22,25 +20,10 @@ public class Grapple : MonoBehaviour
         _lineRenderer.positionCount = 2;
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            //raycast
-            //theres a raycast that will take a bool and set that as true or false depending on the hit
-            RaycastHit2D rayHit = Physics2D.Raycast(transform.position, transform.right, 10.0f, hitLayer);// LayerMask.NameToLayer("Ground"));
-            if (rayHit.collider != null)
-            {
-                Debug.Log("hit wall");
-                _lineRenderer.SetPosition(0, rayHit.collider.transform.position);
-                _lineRenderer.SetPosition(1, transform.position);
-                _distanceJoint.connectedAnchor = rayHit.collider.transform.position;
-            }
-            else
-            {
                 Vector2 mousePos = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
                 _lineRenderer.SetPosition(0, mousePos);
                 _lineRenderer.SetPosition(1, transform.position);
                 _distanceJoint.connectedAnchor = mousePos;
-            }
-            //conditional check, true value, false value
-            Vector3 endPoint = (rayHit.collider != null) ? rayHit.point : transform.position + transform.right * 10.0f;
             _distanceJoint.enabled = true;
             _lineRenderer.enabled = true;
         }
